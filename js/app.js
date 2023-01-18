b325ab9b6ba6a16cfc952b14c050cde09939fbec
K1NGAARON <gerardaaron1997&gmail.com>
2023-01-18 17:06:30 +0100

    changes

--- js/app.js
+++ js/app.js
@@ -3,25 +3,25 @@ $(document).scroll(function () {
     const logo = $('.logo');
 
     if ($(this).scrollTop() > header.height()) {
-        header.addClass('active');
         $(logo).attr("src","/branding/logo-black.svg");
+        header.addClass('active');
     } else {
-        header.removeClass('active');
         $(logo).attr("src","/branding/logo-white.svg");
+        header.removeClass('active');
     }
 });
 
 
-$(window).scroll(function() {
-    const scale = document.querySelector('#scale');
-    console.log(scale);
+// $(window).scroll(function() {
+//     const scale = document.querySelector('#scale');
+//     console.log(scale);
 
-    if (scale) {
-        let mass = Math.min(20, 1+0.005*$(this).scrollTop());
-        console.log(mass);
+//     if (scale) {
+//         let mass = Math.min(20, 1+0.005*$(this).scrollTop());
+//         console.log(mass);
 
-        $('#scale').css('transform', 'scale(' + mass + ')');
-    }
+//         $('#scale').css('transform', 'scale(' + mass + ')');
+//     }
 
 
-});
+// });
