40826832b1b6ab8836c52ec7bc6b191bd5bbaf38
K1NGAARON <gerardaaron1997&gmail.com>
2023-01-23 15:32:45 +0100

    usp script

---
+++ home/js/script.js
@@ -0,0 +1,37 @@
+const usp = [
+    {
+        usp: 'unique-design',
+        body: "We noticed that many of our customers love doing creative campaigns using awesome clothing, but they hate the hassle. That's why we started working on a solution. The Sunday Wardrobe™ is our SaaS-solution, making it super easy for companies to warehouse, handle and ship clothing to key stakeholders.",
+        img: '',
+    },
+    {
+        usp: 'ship-worldwide',
+        body: "Some body text for worldwide shipping",
+        img: '',
+    },
+    {
+        usp: 'worldclass-service',
+        body: "Our very nice CSM team",
+        img: '',
+    }
+];
+
+function toggleUSP(e) {
+    let imageHolder = document.querySelector('#usp-image');
+    let bodyHolder = document.querySelector('#usp-body');
+    let activeUSP = e.target.id;
+
+    $('.usp-toggle').removeClass('active');
+    $(this).addClass('active');
+
+    if (activeUSP == 'unique-design') {
+        bodyHolder.innerHTML = usp[0].body;
+    } else if (activeUSP == 'ship-worldwide') {
+        bodyHolder.innerHTML = usp[1].body;
+    } else if (activeUSP == 'worldclass-service') {
+        bodyHolder.innerHTML = usp[2].body;
+    }
+};
+
+
+$('.usp-toggle').click(toggleUSP);
