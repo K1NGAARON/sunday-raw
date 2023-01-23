812d93f65c00b8cc1901567e75b28c9c2d93e9ce
K1NGAARON <gerardaaron1997&gmail.com>
2023-01-23 13:33:37 +0100

    initial office script

---
+++ contact/js/script.js
@@ -0,0 +1,65 @@
+const offices = [
+    {
+        country: '',
+        city: '',
+        flag: '',
+        location: '',
+        postal: '',
+        img: ''
+    },
+    {
+        country: '',
+        city: '',
+        flag: '',
+        location: '',
+        postal: '',
+        img: ''
+    },
+        {
+        country: '',
+        city: '',
+        flag: '',
+        location: '',
+        postal: '',
+        img: ''
+    },
+        {
+        country: '',
+        city: '',
+        flag: '',
+        location: '',
+        postal: '',
+        img: ''
+    },
+        {
+        country: '',
+        city: '',
+        flag: '',
+        location: '',
+        postal: '',
+        img: ''
+    },
+        {
+        country: '',
+        city: '',
+        flag: '',
+        location: '',
+        postal: '',
+        img: ''
+    },
+        {
+        country: '',
+        city: '',
+        flag: '',
+        location: '',
+        postal: '',
+        img: ''
+    }
+];
+
+function showOffices(e) {
+    const activeOffice = document.querySelector('#offices .active');
+    
+
+
+};
