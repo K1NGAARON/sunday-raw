aa791cba02487f4a407f5fd5a2db106e275bedf1
K1NGAARON <gerardaaron1997@gmail.com>
2023-01-26 16:44:05 +0100

    top selector product pages

--- catalog/hoodies/js/script.js
+++ catalog/hoodies/js/script.js
@@ -1,81 +1,38 @@
-const socialProofTarget = document.querySelector('#social-proof');
-const socialProofContent = [
+const products = [
     {
-        logo: '/img/logos/adyen-logo.svg',
-        body: `
-                Geen zin om jouw werknemers of zakenrelaties dit jaar een doos pralines, een fles cava of een drinkbus cadeau te doen? Kom dit jaar dan origineel uit de hoek met de Streamz Gift Box en geef jouw werknemers een cadeau dat echt entertaint.
-                <br><br>
-                Onze Box heeft het allemaal:
-                <br>
-                ✔️ 1 maand Vlaamse en internationale topseries kijken
-                <br>
-                ✔️ Ideale binge snacks & drinks van bij onz
-                <br>
-                ✔️ Comfy, stijlvolle kousen om je voeten warm te houden
-                <br><br>
-                Een complete entertainmentervaring die je graag cadeau wil doen? Haast je dan naar https://lnkd.in/dYuwH9U5
-                <br><br>
-                Let’s get the Streamz party poppin’! 🥳 
-            `,
-        img: 'https://via.placeholder.com/1200x800'
+        img: '',
     },
     {
-        logo: '/img/logos/adyen-logo.svg',
-        body: `
-                Geen zin om jouw werknemers of zakenrelaties dit jaar een doos pralines, een fles cava of een drinkbus cadeau te doen? Kom dit jaar dan origineel uit de hoek met de Streamz Gift Box en geef jouw werknemers een cadeau dat echt entertaint.
-                <br><br>
-                Onze Box heeft het allemaal:
-                <br>
-                ✔️ 1 maand Vlaamse en internationale topseries kijken
-                <br>
-                ✔️ Ideale binge snacks & drinks van bij onz
-                <br>
-                ✔️ Comfy, stijlvolle kousen om je voeten warm te houden
-                <br><br>
-                Een complete entertainmentervaring die je graag cadeau wil doen? Haast je dan naar https://lnkd.in/dYuwH9U5
-                <br><br>
-                Let’s get the Streamz party poppin’! 🥳 
-            `,
-        img: 'https://via.placeholder.com/1200x800'
+        img: '',
     },
     {
-        logo: '/img/logos/adyen-logo.svg',
-        body: `
-                Geen zin om jouw werknemers of zakenrelaties dit jaar een doos pralines, een fles cava of een drinkbus cadeau te doen? Kom dit jaar dan origineel uit de hoek met de Streamz Gift Box en geef jouw werknemers een cadeau dat echt entertaint.
-                <br><br>
-                Onze Box heeft het allemaal:
-                <br>
-                ✔️ 1 maand Vlaamse en internationale topseries kijken
-                <br>
-                ✔️ Ideale binge snacks & drinks van bij onz
-                <br>
-                ✔️ Comfy, stijlvolle kousen om je voeten warm te houden
-                <br><br>
-                Een complete entertainmentervaring die je graag cadeau wil doen? Haast je dan naar https://lnkd.in/dYuwH9U5
-                <br><br>
-                Let’s get the Streamz party poppin’! 🥳 
-            `,
-        img: 'https://via.placeholder.com/1200x800'
-    }
-];
+        img: '',
+    },
+    {
+        img: '',
+    },
+]
 
-function createSocialProof() {
-    if (socialProofTarget) {
-        shuffleArray(socialProofContent);
+// function selectActiveProduct(e) {
+//     $('.product-image').removeClass('active');
+//     $(this).addClass('active');
+// };
 
-        for (let i = 0; i < socialProofContent.length; i++) {
-            const template = `
-                <div class="card">
-                    <img class="logo" src="${socialProofContent[i].logo}" alt="">
-                    <p>
-                        ${socialProofContent[i].body}
-                    </p>
-                    <img class="featured" src="${socialProofContent[i].img}">
-                </div>
-            `;
-            socialProofTarget.insertAdjacentHTML("beforeend", template);
-        };
-    }
+function showProduct(e) {
+    $('.product-image').removeClass('active');
+    $(this).addClass('active');
+
+    let activeProduct = document.querySelector('.product-image.active');
+    let activeProductSRC = activeProduct.src;
+
+
+    console.log(activeProduct);
+    console.log(activeProductSRC);
+
+    let mainImage = document.querySelector('#main-image');
+    console.log(mainImage);
+
+    $(mainImage).attr('src', activeProductSRC);
 };
 
-$(document).ready(createSocialProof);
+$('.product-image').click(showProduct);
