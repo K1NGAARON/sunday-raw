33439042854b8893955e4a7142f304b48525a035
K1NGAARON <gerardaaron1997@gmail.com>
2023-01-27 21:09:51 +0100

    blog overview page

--- blog/js/cards.js
+++ blog/js/cards.js
@@ -1,54 +1,60 @@
 const target = document.querySelector('.section.content > .row > .wrapper');
 const content = [
     {
-        client: 'Delaware',
-        title: 'High performance sportswear for delaware',
-        body: 'delaware is a global company that delivers advanced ICT solutions and services, guiding our customers through their business and digital transformations.',
-        logo: '/cases/delaware/img/logo.png',
-        img: '/cases/delaware/img/banner.jpg',
-        url: '/cases/delaware/',
-        beachhead: 'HR',
-        companysize: '',
+        title: '5 practical tips on How to put together the Best Swag Bag for your next event',
+        body: "Who doesn't love gifts? Stay top of mind with your prospects, customers and even possible new hires at your next",
+        img: '/blog/best-swag-bag/img/banner.jpg',
+        url: '/blog/best-swag-bag/',
+        beachhead: 'fairs',
     },
     {
-        client: 'Honeypot',
-        title: "Quality wearables pushing Honeypot's corporate gifting to a higher level",
-        body: 'The way Honeypot uses company fashion as a customer gift to improve their management strategy (converting talents into ambassadors).',
-        logo: '/cases/honeypot/img/logo.png',
-        img: '/cases/honeypot/img/banner.jpg',
-        url: '/cases/honeypot/',
-        beachhead: 'Sales',
-        companysize: '',
+        title: 'The Most Effective Employee Retention Strategy in 2023',
+        body: "Employees are an important part of any business. They're often the face of your company, so keeping them happy is key.",
+        img: '/blog/employee-retention-strategy-2023/img/banner.jpg',
+        url: '/blog/employee-retention-strategy-2023/',
+        beachhead: 'hr',
     },
     {
-        client: 'Showpad',
-        title: 'A fit and flexible solution for Showpad',
-        body: "Showing off Showpad's custom made solution to create workplace health by deploying branded sportswear.",
-        logo: '/cases/showpad/img/logo.png',
-        img: '/cases/showpad/img/banner.jpeg',
-        url: '/cases/showpad/',
-        beachhead: 'HR',
-        companysize: '',
+        title: 'The 4 employer branding trends that you should follow',
+        body: "Employer branding is the secret to attracting the best talent from within the industry.",
+        img: '/blog/employer-branding-trends/img/banner.jpg',
+        url: '/blog/employer-branding-trends/',
+        beachhead: 'hr',
     },
     {
-        client: 'ION',
-        title: "ION knows how to motivate a team: #actION please!",
-        body: "body incoming",
-        logo: '/cases/ion/img/logo.svg',
-        img: '/cases/ion/img/banner.jpeg',
-        url: '/cases/ion/',
-        beachhead: 'HR',
-        companysize: '',
+        title: 'How long does it take to onboard a new employee?',
+        body: "If you’re looking to hire new employees, it‘s important to understand how long it will take to onboard them. There are several reasons why knowing this timeframe is beneficial.",
+        img: '/blog/how-long-to-onboard-employee/img/banner.jpg',
+        url: '/blog/how-long-to-onboard-employee/',
+        beachhead: 'hr',
     },
     {
-        client: 'Silverfin',
-        title: 'Learn how Silverfin increased performance and wellbeing for their employees',
-        body: 'tbaaaaaa',
-        logo: '/cases/silverfin/img/logo.png',
-        img: '/cases/silverfin/img/banner.jpeg',
-        url: '/cases/silverfin/',
-        beachhead: 'HR',
-        companysize: '',
+        title: 'How to make your employees happy and healthy',
+        body: "The wellbeing of employees throughout the organization will be significantly improved by taking note of your team's needs.",
+        img: '/blog/make-employees-happy/img/banner.jpg',
+        url: '/blog/make-employees-happy/',
+        beachhead: 'hr',
+    },
+    {
+        title: '7 Ways to Make Your Next Conference or Event More Sustainable',
+        body: "There’s no denying that events have an impact on our environment.",
+        img: '/blog/make-your-event-more-sustainable/img/banner.jpg',
+        url: '/blog/make-your-event-more-sustainable/',
+        beachhead: 'fairs',
+    },
+    {
+        title: 'What are Pantone colors?',
+        body: "The basic elements of a good branding are: a catchy name, a recognizable logo, a clear typography and popping colors.",
+        img: '/blog/what-are-pantone-colors/img/banner.jpg',
+        url: '/blog/what-are-pantone-colors/',
+        beachhead: 'design',
+    },
+    {
+        title: 'Why is the onboarding process important?',
+        body: "In the world of human resources, onboarding is a term many are familiar with. But when you step outside of HR, candidates and new hires may not have a clear understanding of what exactly the onboarding process involves.",
+        img: '/blog/why-is-onboarding-important/img/banner.png',
+        url: '/blog/why-is-onboarding-important/',
+        beachhead: 'hr',
     }
 ];
 
@@ -57,7 +63,6 @@ function createCards() {
         const template = `
             <div class="card ${content[i].beachhead.toLowerCase()}">
                 <img src="${content[i].img}" alt="${content[i].title}">
-                <img class="logo" src="${content[i].logo}" alt="${content[i].title}">
                 <h5>
                     ${content[i].title}
                 </h5>
@@ -65,7 +70,7 @@ function createCards() {
                     ${content[i].body}
                 </p>
                 <a class="btn" href="${content[i].url}">
-                    Read the case
+                    Read the blog
                 </a>
             </div>
         `;
