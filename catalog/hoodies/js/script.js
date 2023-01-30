14c8db855438fad346282dde70e3a5f192a13e11
K1NGAARON <gerardaaron1997&gmail.com>
2023-01-30 11:20:58 +0100

    changes

--- catalog/hoodies/js/script.js
+++ catalog/hoodies/js/script.js
@@ -13,11 +13,6 @@ const products = [
     },
 ]
 
-// function selectActiveProduct(e) {
-//     $('.product-image').removeClass('active');
-//     $(this).addClass('active');
-// };
-
 function loadProducts(e) {
     
 };
@@ -39,6 +34,29 @@ function showProduct(e) {
     $(mainImage).attr('src', activeProductSRC);
 };
 
-$(document).ready(loadProducts);
+
+const stepsContent = [
+    {
+        title: "Brand guidelines",
+        body: "The first step before anything gets designed is having a look at your brand guidelines. Logo, colors, typography, … everything that makes you unique.",
+        img: '',
+    },
+    {
+        title: "Fashion trends",
+        body: "Our design department follows up on fashion trends and translates them in wearable designs that match your brand identity. That way we create clothing that is on-brand and on-trend.",
+        img: '',
+    },
+    {
+        title: "Target audience",
+        body: "Depending on the target audience, the design and fit of the clothing will be different. We want the people who receive your clothes to become real ambassadors of your brand and that they can wear it as often as possible. This will only work if we match the product, design and fitting to your target audience.",
+        img: '',
+    },
+    {
+        title: "Clothing purpose",
+        body: "What would you like to reach with this collection? From brand awareness to getting your team adopt a more healthy lifestyle, we guide you through the best products and materials.",
+        img: '',
+    },
+];
+
 
 $('.product-image').click(showProduct);
