9674f740c77980f4b6ad6f62c2dd8148656b4582
K1NGAARON <gerardaaron1997@gmail.com>
2023-02-03 16:33:53 +0100

    added sample pop up to hoodies

--- catalog/js/script.js
+++ catalog/js/script.js
@@ -142,6 +142,26 @@ function createSocialProof() {
     }
 };
 
+// Get the modal
+let modal = document.getElementById("myModal");
+let modalBtn = document.getElementById("sampleBtn");
+let span = document.getElementById("closeModal");
+
+// When the user clicks on the button, open the modal
+modalBtn.addEventListener('click', function() {
+    modal.style.display = "block";
+});
+
+span.addEventListener('click', function(e) {
+    modal.style.display = "none";
+});
+
+window.addEventListener('click', function(e) {
+    if (e.target == modal) {
+        modal.style.display = "none";
+      }
+});
+
 $(document).ready(createSocialProof);
 $('.step').click(changeSteps);
 $('.product-image').click(showProduct);
