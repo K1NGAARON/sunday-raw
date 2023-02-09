98922900183fe0c8f847c7fdfc130b0157b99be9
K1NGAARON <gerardaaron1997@gmail.com>
2023-02-09 12:01:58 +0100

    collections

---
+++ collections/js/script.js
@@ -0,0 +1,53 @@
+const collectionTarget = document.querySelector('.collections .wrapper');
+console.log(collectionTarget);
+
+const collectionsContent = [
+    {
+        title: "Eye-catching influencers for Ablo",
+        body: "",
+        url: '/collections/ablo/',
+        img: '/collections/ablo/img/banner.jpeg'
+    },
+    {
+        title: "Local Heroes of Brooklyn",
+        body: "",
+        url: '/collections/brooklyn/',
+        img: '/collections/brooklyn/img/banner.jpeg'
+    },
+    {
+        title: "John&Jane, Bold Never Regular",
+        body: "",
+        url: '/collections/john-and-jane/',
+        img: '/collections/john-and-jane/img/banner.jpeg'
+    },
+    {
+        title: "The collection of Celebration",
+        body: "",
+        url: '/collections/johnnie-walker/',
+        img: '/collections/johnnie-walker/img/banner.jpeg'
+    },
+    {
+        title: "Oaky, Vivid in Violet",
+        body: "",
+        url: '/collections/oaky/',
+        img: '/collections/oaky/img/banner.jpeg'
+    },
+    {
+        title: "Showpadders kicking ass at its brightest",
+        body: "",
+        url: '/collections/showpad/',
+        img: '/collections/showpad/img/banner.jpeg'
+    },
+    {
+        title: "Your favorite dash of paint",
+        body: "",
+        url: '/collections/trimetal/',
+        img: '/collections/trimetal/img/banner.jpeg'
+    },
+    {
+        title: "Value Collection 21 by Uberall",
+        body: "",
+        url: '/collections/uberall/',
+        img: '/collections/uberall/img/banner.jpeg',
+    },
+]
