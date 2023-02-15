c60a3e791ee807a61af0429a061f1614760f424c
K1NGAARON <gerardaaron1997&gmail.com>
2023-02-15 13:41:15 +0100

    freebies page

--- freebies/js/script.js
+++ freebies/js/script.js
@@ -1,15 +1,15 @@
 const freebies = [
     {
         title: "Fashion trend report for businesses in 2022",
-        body: "",
+        body: "Anyone who thinks that corporate clothing is always boring and often of poor quality should meet Sunday.",
         img: '/freebies/trend-report/img/trend-report.png',
-        url: ''
+        url: '/freebies/trend-report/'
     },
     {
         title: "Download our lookbook",
-        body: "",
-        img: '',
-        url: ''
+        body: "We see ourselves as architects of our clients' fashion collections. We help them design on-brand and on-trend amazing clothing items that really fit their culture.",
+        img: '/freebies/lookbook/img/lookbook.png',
+        url: '/freebies/lookbook/'
     },
 ];
 
@@ -18,14 +18,22 @@ function createCards() {
 
     for (let i = 0; i < freebies.length; i++) {
         const template = `
-        <div>
-            ${freebies[i].title}
+        <div class="card">
+            <img src="${freebies[i].img}">
+            <h5>
+                ${freebies[i].title}
+            </h5>
+            <p>
+                ${freebies[i].body}
+            </p>
+            <a class="btn" href="${freebies[i].url}">
+                More info
+            </a>
             
         </div>
-        <img src="${freebies[i].img}">
         `;
         target.insertAdjacentHTML("afterbegin", template);
     };
 };
 
-// $(document).ready(createCards);
+$(document).ready(createCards);
