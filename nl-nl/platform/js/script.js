deb4d8a669ea32ebeab7a5554a3aaae77dc4af89
K1NGAARON <gerardaaron1997&gmail.com>
2023-03-13 15:49:13 +0100

    cleaned image paths

--- nl-nl/platform/js/script.js
+++ nl-nl/platform/js/script.js
@@ -2,32 +2,32 @@ const content = [
     {
         title: "Het is kostenbesparendmoney",
         body: "Door alle inspanningen met betrekking tot merchandise te bundelen in plaats van veel verschillende initiatieven te ondernemen, besparen onze klanten doorgaans tot 50% per jaar aan kosten.",
-        img: './img/usp/save.png',
+        img: '/platform/img/usp/save.png',
     },
     {
         title: "Het is veel efficiënter",
         body: "De rompslomp van het organiseren van merchandise voor jouw team, zijn eindelijk voorbij. je kan het overzicht van de kleding elk moment raadplegen, beheren of verzenden in enkele kliks.",
-        img: './img/usp/save-time.png',
+        img: '/platform/img/usp/save-time.png',
     },
     {
         title: "Het genereert data inzicht",
         body: "Omdat alles gecentraliseerd is, is het erg makkelijk om veel datapunten te verzamelen, die zich vervolgens vertalen in waardevolle inzichten, toegankelijk vanaf het platform.",
-        img: './img/usp/data.png',
+        img: '/platform/img/usp/data.png',
     },
     {
         title: "Zero waste aanpak",
         body: "Een beurs of evenement gehad waar je minder items hebt gebruikt dan voorzien? Geen probleem, zet de producten terug in je Virtual Wardrobe en gebruik ze voor een andere campagne.",
-        img: './img/usp/zero-waste.png',
+        img: '/platform/img/usp/zero-waste.png',
     },
     {
         title: "Het is GDPR-compliant",
         body: "Zodra je het platform gaat gebruiken, komen er veel gevoelige gegevens in de database terecht. Maak je echter geen zorgen: de gegevens zijn alleen toegankelijk voor degenen die daartoe bevoegd zijn.",
-        img: './img/usp/gdpr.png',
+        img: '/platform/img/usp/gdpr.png',
     },
     {
         title: "Merkconsistentie",
         body: "Door je team toegang te geven tot de centraal opgeslagen collectie(s), zorg je ervoor dat ze alleen items gebruiken die 100% consistent en on-brand zijn.",
-        img: './img/usp/consistent.png',
+        img: '/platform/img/usp/consistent.png',
     }
 ];
 
