1f1279b83436275e35321ed0e13884e847ab796a
K1NGAARON <gerardaaron1997@gmail.com>
2023-03-30 15:41:26 +0200

    changes

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,6 @@
 const mix = require('laravel-mix');
 
 mix.js('js/splide.js', 'js/dist/splide.js')
-    .js('js/app.js', 'js/dist/app.js');
+    .js('/nl-nl/js/splide.js', '/nl-nl/js/dist/splide.js')
+    .js('js/app.js', 'js/dist/app.js')
+    .js('/nl-nl/js/app.js', '/nl-nl/js/dist/app.js');
