02637e88d583e14f0c86256c52172ac827b610de
K1NGAARON <gerardaaron1997@gmail.com>
2023-04-26 14:37:32 +0200

    changes

--- catalog/summer-boxes/js/script.js
+++ catalog/summer-boxes/js/script.js
@@ -2,7 +2,7 @@ function toggleButton(e) {
     $(this).toggleClass('active');
 
     if ($(this).hasClass("active")) {
-        $(this).text('Remove from cart');
+        $(this).text('Remove from box');
     } else {
         $(this).text('Add to cart');
     }
@@ -12,6 +12,8 @@ function toggleButton(e) {
 function addProducts() {
     const form = document.querySelector('.contact-form form');
     const addToBoxBtns = document.querySelectorAll(".item .btn");
+    const estimatedPriceElement = document.querySelector('.estimated-price');
+    let estimatedPrice = 0;
   
     // Loop through each button and attach a click event listener
     addToBoxBtns.forEach(btn => {
@@ -20,6 +22,7 @@ function addProducts() {
   
     function handleAddToBoxClick() {
         const productName = this.parentNode.querySelector("h5").textContent.trim();
+        const productPrice = parseFloat(this.dataset.price);
         
         // Find the checkbox field in the form
         const checkboxField = form.querySelectorAll('[name="product_interest"]');
@@ -28,12 +31,19 @@ function addProducts() {
             if (checkbox.value === productName) {
                 if (checkbox.checked === false) {
                     checkbox.checked = true;
+                    estimatedPrice += productPrice;
                 } else {
                     checkbox.checked = false;
+                    estimatedPrice -= productPrice;
                 }
             }
         });
+    
+        // Update the counter element
+        const counter = document.querySelector(".counter");
+        counter.textContent = `${estimatedPrice.toFixed(2)}`;
     }
+    
       
     // Attach click event listener to all "Add to box" buttons
     addToBoxBtns.forEach(btn => {
@@ -42,6 +52,7 @@ function addProducts() {
 }
   
 window.addEventListener("load", addProducts, { once: true });
+
   
 
   
