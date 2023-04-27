87604777bf2d40390e5093a6c248e867e55859c2
K1NGAARON <gerardaaron1997@gmail.com>
2023-04-27 14:10:20 +0200

    canegs

--- partners/js/new.js
+++ partners/js/new.js
@@ -12,9 +12,7 @@ const usp = [
         usp: 'ship-worldwide',
         body: "Sunday is a team of textile experts. We focus on wearables and accessories, starting from scratch. Because of our full custom offering and expertise, we go all the way in accomplishing high quality products to make your project a huge success.  We'll guide you from end to start and communicate transparently during the proces.",
         video: `
-            <div class="video-wrapper side" id="videoHolder">
-                <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/794577698?h=db554bdd29&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="marieke.mp4"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
-            </div>
+            <img style="border-radius: 26px;" src="/partners/img/design.jpg" alt="Design Support">
         `,
     },
     {
