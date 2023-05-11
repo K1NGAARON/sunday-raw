c75c4648851867591bbddd97425c989ba39edfd2
K1NGAARON <gerardaaron1997@gmail.com>
2023-05-11 16:53:04 +0200

    fixed img

--- cases/js/cards.js
+++ cases/js/cards.js
@@ -135,7 +135,7 @@ const content = [
         title: "How Deliverect skyrockets their company culture globally, using the Sunday Wardrobe™",
         body: "Deliverect is growing at a fast pace by raising funding. As a result, there is an internal need to ensure that all employees continue to feel connected to the company and the mission.",
         logo: '/cases/deliverect/img/logo.png',
-        img: '/cases/deliverect/img/banner.jpg',
+        img: '/cases/deliverect-culture/img/banner.jpg',
         url: '/cases/deliverect-culture/',
         beachhead: 'hr',
         companysize: '',
