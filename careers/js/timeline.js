fa2c828272e2759ce1669666ffdecdeace262a9d
K1NGAARON <gerardaaron1997@gmail.com>
2023-06-07 14:24:03 +0200

    chanegs

---
+++ careers/js/timeline.js
@@ -0,0 +1,67 @@
+
+// Timeline component
+let timelineToggle = document.querySelectorAll(".steps-toggle");
+
+function removeActiveTimeline() {
+    for(let i = 0; i < timelineToggle.length; i++) {
+        timelineToggle[i].classList.remove("active");
+    }
+};
+
+$('.single-item').click(function(e){
+    removeActiveTimeline();
+    $(this).find('.steps-toggle').toggleClass('active');
+});
+
+
+const timelineSteps = [
+    {
+        stepTitle : "Interview HR team",
+        stepBody : "We received your resume and motivation letter: our international recruiter will set up a first meeting with you to get to know you better and talk about your future goals."
+    },
+    {
+        stepTitle : "Interview direct manager",
+        stepBody : "You did well, we feel there could be a match: meet your (future) manager."
+    },
+    {
+        stepTitle : "Interview founders",
+        stepBody : "Our founders love to meet you and see if they feel the match as well."
+    },
+    {
+        stepTitle : "Welcome aboard!",
+        stepBody : "Welcome aboard! We will guide you through your first days with an entire onboarding process."
+    },
+]
+
+let step1Toggle = document.querySelector("#step-1");
+let step2Toggle = document.querySelector("#step-2");
+let step3Toggle = document.querySelector("#step-3");
+let step4Toggle = document.querySelector("#step-4");
+
+// Timelineholder 
+let timelineTitle = document.querySelector("#body-title");
+let timelineBody = document.querySelector("#body-text");
+
+// Step 1
+step1Toggle.addEventListener("click", function() {
+    timelineTitle.innerHTML = timelineSteps[0].stepTitle;
+    timelineBody.innerHTML = timelineSteps[0].stepBody;
+});
+
+// Step 2
+step2Toggle.addEventListener("click", function() {
+    timelineTitle.innerHTML = timelineSteps[1].stepTitle;
+    timelineBody.innerHTML = timelineSteps[1].stepBody;
+});
+
+// Step 3
+step3Toggle.addEventListener("click", function() {
+    timelineTitle.innerHTML = timelineSteps[2].stepTitle;
+    timelineBody.innerHTML = timelineSteps[2].stepBody;
+});
+
+// Step 4
+step4Toggle.addEventListener("click", function() {
+    timelineTitle.innerHTML = timelineSteps[3].stepTitle;
+    timelineBody.innerHTML = timelineSteps[3].stepBody;
+});
