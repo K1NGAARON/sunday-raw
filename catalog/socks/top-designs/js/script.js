fabd11a574e65ac3eb5c4dd0f43b49ca3d9e90f7
K1NGAARON <gerardaaron1997@gmail.com>
2023-10-19 11:49:46 +0200

    changes

--- catalog/socks/top-designs/js/script.js
+++ catalog/socks/top-designs/js/script.js
@@ -2,6 +2,13 @@ document.addEventListener( 'DOMContentLoaded', function() {
     new Splide( '#gallery-slides', {
         heightRatio: 0.5,
         perPage    : 3,
+        breakpoints: {
+            640: {
+                heightRatio: 1,
+                perPage: 2,
+            },
+        },
         type: 'loop',
+        
     }).mount();
 });
