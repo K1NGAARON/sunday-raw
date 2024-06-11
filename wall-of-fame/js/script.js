861ead96d164e95d54bb657acac9921988a32a12
K1NGAARON <gerardaaron1997@gmail.com>
2024-06-11 10:05:42 +0200

    wll of fame page

---
+++ wall-of-fame/js/script.js
@@ -0,0 +1,47 @@
+const galleryWrapper = document.querySelector('#gallery');
+
+const reviews = []
+
+// Size of Reviews Folder (+1)
+const reviewsLength = 48;
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+function createGalleryArray() {
+    for (let i = 1; i < reviewsLength; i++) {
+        let location = '/wall-of-fame/img/review-' + i + '.png';
+        
+        let testimonialObject = {
+            'src' : location
+        };
+        
+        reviews.push(testimonialObject);
+    };
+    
+    shuffleArray(reviews);
+} 
+
+function createGallery() {
+    for (let i = 0; i < reviews.length; i++) {
+        const galleryIMG = `
+            <div class="item">
+                <img src="${reviews[i].src}" alt="Sunday Review">
+            </div>
+        `;
+
+        galleryWrapper.insertAdjacentHTML("beforeend", galleryIMG);
+    };
+};
+
+
+$(document).ready(function() {
+    createGalleryArray();
+    createGallery();
+});
