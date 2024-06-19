89bfc55892374ba63d6a2770b2f8145fc1a67d93
K1NGAARON <gerardaaron1997@gmail.com>
2024-06-19 16:53:41 +0200

    changes

--- wall-of-fame/js/script.js
+++ wall-of-fame/js/script.js
@@ -3,7 +3,7 @@ const galleryWrapper = document.querySelector('#gallery');
 const reviews = []
 
 // Size of Reviews Folder (+1)
-const reviewsLength = 14;
+const reviewsLength = 71;
 
 function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
@@ -16,7 +16,7 @@ function shuffleArray(array) {
 
 function createGalleryArray() {
     for (let i = 1; i < reviewsLength; i++) {
-        let location = '/wall-of-fame/img/review-' + i + '.png';
+        let location = '/wall-of-fame/img/Review-' + i + '.png';
         
         let testimonialObject = {
             'src' : location
