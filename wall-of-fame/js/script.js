742d6d1773969700add5cfb242718c7d4a03dc03
K1NGAARON <gerardaaron1997@gmail.com>
2024-06-19 16:25:19 +0200

    changes

--- wall-of-fame/js/script.js
+++ wall-of-fame/js/script.js
@@ -3,7 +3,7 @@ const galleryWrapper = document.querySelector('#gallery');
 const reviews = []
 
 // Size of Reviews Folder (+1)
-const reviewsLength = 48;
+const reviewsLength = 71;
 
 function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
