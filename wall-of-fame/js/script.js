b66980f4f71b11fa6c98e62acbae08d653e4f5d4
K1NGAARON <gerardaaron1997@gmail.com>
2024-06-26 11:30:04 +0200

    changes

--- wall-of-fame/js/script.js
+++ wall-of-fame/js/script.js
@@ -16,7 +16,7 @@ function shuffleArray(array) {
 
 function createGalleryArray() {
     for (let i = 1; i < reviewsLength; i++) {
-        let location = '/wall-of-fame/img/Review-' + i + '.png';
+        let location = '/wall-of-fame/img/review-' + i + '.png';
         
         let testimonialObject = {
             'src' : location
